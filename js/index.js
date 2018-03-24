882c68f6de8e60b0401c6fd7b38448eca43e8567
loi.wu <loi.wu@vipshop.com>
2018-03-24 22:34:02 +0800

    make it better

--- js/index.js
+++ js/index.js
@@ -1,237 +1,234 @@
 $(document).ready(function() {
-  var divx = document.getElementById('terminal');
-  /* Welcome screen */
-  $('#welcome-login').animate({'opacity': '1', 'top': $(window).height()/2 - $('#welcome-login').height()/2 }, 3000);
+	
+	
+	
+	var basicCommands = {
+		help: {
+			description: 'provides a list of commands with their description to the user.',
+			parameter: 'the command to get help about',
+			action: '',
+		},
+	};
+	
+	var userCommands = {
+		connect: { // the command itself
+			description: 'connects the device to the nearest phone relay', // the description that appears in the help menu for this command
+			parameterlist: { // the list of parameters for this command
+				parameter1: {
+					description: 'the target to connect to', // description of the parameter
+					optional: false, // if the parameter has to be mandatory (default: optional)
+				},
+				parameter2: {
+					parameter: '-s', // if the parameter has to be a certain string
+					description: 'connect to target with the secure connection',
+				},
+			},
+			actionlist: { // if the command should take more than one actions
+				action1: {
+					text: 'connecting to #{parameter1}...', // the text it should show for this action
+					time: 3, // the time it should take to execute the next action
+				},
+				action2: {
+					text: 'connected to #{parameter1}',
+				},
+			},
+		},
+		disconnect: {
+			description: 'disconnect from the currently connected target',
+			actionList: {
+				action1: {
+					text: 'disconnected',
+				},
+			},
+		},
+	};
+	
+	var commands = Object.assign(basicCommands, userCommands);
+	
+	var mission = {
+		type: 'mission',
+		entry1: {
+			chat: '/', // in which chat should the text appear
+			text: 'hey there #{nickname}', // text that appears in the chat
+			time: 10, // after how many seconds should the script continue to the next entry
+		},
+		entry2: {
+			chat: '/',
+			text: 'lets get used to this interface, shall we',
+			time: 30,
+		},
+		entry3: {
+			chat: '/',
+			text: 'if you need any help just type in help anytime and you will find a bunch of useful commands that you can use in your terminal/console/command prompt or whatever you want to call it',
+			time: 10,
+		},
+		entry4: {
+			chat: '/',
+			text: 'try it out now',
+			command: 'help', // which command should trigger the script to continue to the next entry
+		},
+		entry5: {
+			chat: '/',
+			text: 'there you go',
+			time: 10,
+		},
+		entry6: {
+			chat: '/',
+			text: 'now lets try something different',
+			time: 20,
+		},
+		entry7: {
+			chat: '/',
+			text: 'try to connect to the nearest phone relay',
+			command: 'connect',
+		},
+		entry8: {
+			chat: '/',
+			text: 'now disconnect before you get tracked down. you have 60 seconds before they track you down...',
+			command: 'disconnect',
+			time: 60, // if command property exists as well, time shows after how many seconds does it take to happen the game over
+		},
+		entry9: { // if neither command nor time property exist, it is the end of the mission
+			chat: '/',
+			text: 'congratulation! you just passed our test!',
+		},
+	};
+	
+	
+	
+	var chat = {
+		'/': [
+			'hey there #{nickname}',
+			'lets get used to this interface, shall we?',
+			'if you need any help just type in help anytime and you will find a bunch of useful commands that you can use in your terminal/console/command prompt or whatever you want to call it',
+			'alright, so we hope you\'ve got used to the interface by now...',
+		],
+		'Anonymous': [
+			'hey there #{nickname}',
+		],
+		'CIA': [
+			'hey there #{nickname}',
+		],
+	};
+	var objectives = [];
+	
+	
+	
+	console.clear();
+	
+	var nickname = 'Doe';
 
-  setTimeout(function() {
-      $('#login-input').animate({'opacity': '1'}, 3000);
-  }, 4000);
+	var keyCode = {
+		enter: 13,
+		arrowUp: 38,
+		arrowDown: 40,
+		leftArrow: 37,
+		rightArrow: 39
+	};
 
-  /* player login */
-  var player_name = "Anonymous";
-  var logged = 0;
+	var inputHistory = {
+		backward: [],
+		forward: []
+	};
+	
+	var $gameContainer = $('#game');
+	var $consoleInput = $('#console-input');
+	var $display1 = $('#display1');
+	var $consoleVisibleInput = $('#console-visible-input');
+	var $startingScreen = $('#starting-screen');
+	var $nicknameInput = $('#nickname');
+	
+	var $objectivesContainer = $('#objectives');
 
-  $('#login-input').keypress(function(e) {
-    if(e.which === 13 && $(this).val() != '') {
-      player_name = $(this).val();
-      logged = 1;
-      $('#welcome-login').animate({'opacity': '0'}, 1000);
-      /* objectives */
-      var help = 0;
-      var list = 0;
-      var connect = 0;
-      var trojan = 0;
-      var disconnect = 0;
+	setTimeout(function() {
+		$startingScreen.addClass('showing');
+	}, 1000);
+	
+	$nicknameInput.on('keyup', function(e) {
+		if ($nicknameInput.val() !== '' && e.which === keyCode.enter) {
+      nickname = $nicknameInput.val();
+			$startingScreen.removeClass('showing');
+			$gameContainer.addClass('showing');
+			var i = 0;
+			var chatInterval = setInterval(function () {
+				$objectivesContainer.append('<li> ' + chat['/'][i].replace('#{nickname}', nickname) + '</li>');
+				i++;
+				if (i >= chat['/'].length) {
+					clearInterval(chatInterval);
+				}
+			}, 3000);
+		}
+	});
 
-      var won = 0;
-      var able = 0;
+	function onEnter(input) {
+		if (inputHistory.backward.length >= 50) {
+			inputHistory.backward.shift();
+		}
+		inputHistory.backward.push(input);
+		if (inputHistory.forward.length > 0) {
+			inputHistory.forward = [];
+		}
+		$display1.append('<div>' + input + '</div>');
+		$consoleInput.val('');
+		switch(input) {
+			case 'help':
+				$display1.append('<div>Help...</div>');
+				break;
+			case 'cls':
+				$display1.find('div:not(#console-input)').remove();
+				break;
+			case 'exit':
+				$display1.append('<div>Shutting down HACKSYS...</div>');
+				break;
+			default:
+				$display1.append('<div>[' + input + '] command not recognized. Type help for a list of available commands.</div>');
+		}
+		$consoleVisibleInput.remove();
+		$display1.append('<div id="console-visible-input"><div id="console-caret">&nbsp</div></div>');
+		$consoleVisibleInput = $('#console-visible-input');
+		$('#display1')[0].scrollTop = $('#display1')[0].scrollHeight;
+	}
 
-      window.setInterval(function() {
-        if (won === 0) {
-          if (help === 1) {
-            $('#objectives #help').css({'color': 'red'});
-            $('#objectives #list').animate({'opacity': '1'}, 1000);
-            if (list === 1) {
-              $('#objectives #list').css({'color': 'red'});
-              $('#objectives #connect').animate({'opacity': '1'}, 1000);
-              if (connect === 1) {
-                $('#objectives #connect').css({'color': 'red'});
-                $('#objectives #trojan').animate({'opacity': '1'}, 1000);
-                if (trojan === 1) {
-                  $('#objectives #trojan').css({'color': 'red'});
-                  $('#objectives #disconnect').animate({'opacity': '1'}, 1000);
-                }
-              }
-            }
-          }
-        }
+	function onArrowUp() {
+		if (inputHistory.backward.length > 0) {
+			inputHistory.forward.push($consoleInput.val());
+				$consoleInput.val(inputHistory.backward[inputHistory.backward.length - 1]);
+			inputHistory.backward.pop();
+		}
+		$consoleVisibleInput.html($consoleInput.val() + '<div id="console-caret">&nbsp</div>');
+	}
 
-        if (disconnect === 1) {
-          $('#objectives #disconnect').css({'color': 'red'});
-        }
+	function onArrowDown() {
+		if (inputHistory.forward.length > 0) {
+			inputHistory.backward.push($consoleInput.val());
+			$consoleInput.val(inputHistory.forward[inputHistory.forward.length - 1]);
+			inputHistory.forward.pop();
+		}
+		$consoleVisibleInput.html($consoleInput.val() + '<div id="console-caret">&nbsp</div>');
+	}
+	
+	$display1.on('click', function() {
+		$consoleInput.focus();
+	});
 
-        if (minutes <= 1 && help === 1 && list === 1 && connect === 0 && trojan === 1 && disconnect === 1) {
-          won++;
-        }
-
-        if (won === 1) {
-          $('#objectives li').animate({'opacity': '0'}, 3000);
-          $('#objectives #won').show(3000).animate({'opacity': '1'}, 3000);
-        } else if (won > 1) {
-          won = 2;
-        }
-      }, 1000);
-
-      /* timer */
-      var seconds = 0;
-      var minutes = 0;
-      var danger = 0;
-      var lost = 0;
-
-      window.setInterval(function() {
-        if (connect === 1) {
-          if (seconds < 10) {
-            $('#time').text('Being traced: ' + minutes + ':0' +seconds);
-          } else {
-            $('#time').text('Being traced: ' + minutes + ':' +seconds);
-          }
-          if (seconds < 59) {
-            seconds++;
-          } else {
-            seconds = 0;
-            minutes++;
-          }
-          if (minutes >= 0 && seconds > 45) {
-            $('#time').css({'color': 'red'});
-            danger++;
-          }
-          if (danger === 1) {
-            $('span').remove();
-            $('#terminal').append('<div>You have only 15 seconds left...<span id="blinking">_</span></div><br>');
-            divx.scrollTop = divx.scrollHeight;
-          } else if (danger > 1) {
-            danger = 2;
-          }
-          if (minutes >= 1 && seconds > 0) {
-            $('#time').text('You\'ve been traced down!');
-            lost++;
-            able = 0;
-          }
-          if (lost === 1) {
-            $('span').remove();
-            $('#terminal').append('<div>You\'ve been traced down!<br>Formatting HDD...<br>Goodbye...<span id="blinking">_</span></div><br>');
-            $('#objectives li').animate({'opacity': '0'}, 3000);
-            $('#objectives #lost').show(3000).animate({'opacity': '1'}, 3000);
-            divx.scrollTop = divx.scrollHeight;
-            $('#objectives li').css({'color': 'green'}).animate({'opacity': '1'}, 3000);
-            setTimeout(function() {
-              $('#terminal').animate({'opacity': '0'}, 1000);
-              $('#root').animate({'opacity': '0'}, 1000);
-              $('input').animate({'opacity': '0'}, 1000).hide(3000);
-              $('#objectives').animate({'opacity': '0'}, 1000);
-              $('#time').animate({'opacity': '0'}, 1000);
-              help = 0;
-              list = 0;
-              connect = 0;
-              trojan = 0;
-              disconnect = 0;
-              won = 0;
-              $('#objectives li').css({'color': 'green'});
-            }, 20000);
-          } else if (lost > 1) {
-            lost = 2;
-          }
-        }
-      }, 1000);
-
-      setTimeout(function(){
-        $('#terminal').animate({'opacity': '1'}, 1000);
-      }, 1000);
-
-      setTimeout(function(){
-        $('#root').animate({'opacity': '1'}, 1000);
-        $('input').animate({'opacity': '1'}, 1000).show();
-      }, 5000);
-
-      setTimeout(function(){
-        $('span').remove();
-        $('#terminal').append('<br><div>Welcome to HACKSYS [version 1.0.0].<br>(c) Copyright 2014 HACorp Corporation. All rights reserved.<br><br>' + player_name + ' authenticated.<br>HACKSYS ready for use.<br>Use the help command for a list of commands.<br><br><span id="initial-root">$</span>&nbsp;&nbsp;&nbsp;<span id="blinking">_</span></div>');
-      }, 6500);
-
-      setTimeout(function() {
-        $('#time').animate({'opacity': '1'}, 3000);
-        $('#objectives').animate({'opacity': '1'}, 3000);
-      }, 7000);
-
-      setTimeout(function() {
-        $('#objectives #help').animate({'opacity': '1'}, 500);
-        able = 1;
-      }, 10000);
-
-      $('#input').keypress(function(e) {
-          if(e.which === 13 && $(this).val() != '' && able === 1) {
-            $('div span').remove();
-            $('initial-root').remove();
-
-            /* 
-             * List of commands available in the game:
-             * 
-             * help
-             * list
-             * connect nearest-phone-relay
-             * send trojan
-             * send adware
-             * disconnect
-             * exit
-             * 
-             */
-
-            switch( $(this).val() ) {
-              case 'help':
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>List of commands:<br>help - Show this message (command list)<br>connect [DNS name] - Connects host computer with the targeted DNS name<br>disconnect - Disconnects host computer from the connected DNS name<br>exit - Shuts down host computer<br>list - Shows the list of the currently available applications<br>send [application type] - Sends given application name to the connected DNS name<span id="blinking">_</span></div>');
-                help = 1;
-                break;
-              case 'connect nearest.phone.relay':
-                if (connect === 0) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Connecting...<br>Connected.<br>You have 1 minute before you get traced down.<span id="blinking">_</span></div>');
-                  connect = 1;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are already connected to nearest.phone.relay.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'list':
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>List of currently available applications:<br>AdBreak - bombard targeted computer with advertisement pop-ups (type: adware)<br>Trojan.Vaklik.BBB - steals vital information from the targeted computer (type: trojan)<span id="blinking">_</span>');
-                list = 1;
-                break;
-              case 'send trojan':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Sending Trojan.Vaklik.BBB...<br>Trojan.Vaklik.BBB sent.<span id="blinking">_</span></div>');
-                  trojan = 1;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'send adware':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Sending AdBreak...<br>Unable to send AdBreak.<span id="blinking">_</span></div>');
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'disconnect':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Disconnecting...<br>Disconnected<span id="blinking">_</span></div>');
-                  disconnect = 1;
-                  connect = 0;
-                  minutes = 0;
-                  seconds = 0;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'exit':
-                $('#terminal').animate({'opacity': '0'}, 1000);
-                $('#root').animate({'opacity': '0'}, 1000);
-                $('input').animate({'opacity': '0'}, 1000).hide(3000);
-                $('#objectives').animate({'opacity': '0'}, 1000);
-                $('#time').animate({'opacity': '0'}, 1000);
-                help = 0;
-                list = 0;
-                connect = 0;
-                trojan = 0;
-                disconnect = 0;
-                won = 0;
-                $('#objectives li').css({'color': 'green'});
-                break;
-              default:
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Command not recognized. Type in help for a list of commands.<span id="blinking">_</span></div>');
-            }
-            $('#terminal').append('<br>');
-
-            divx.scrollTop = divx.scrollHeight;
-            $(this).val('');
-          }
-      });
-    }
-  });
+	$consoleInput.on('keyup', function(e) {
+		$consoleVisibleInput.html($(this).val() + '<div id="console-caret">&nbsp</div>');
+		switch (e.which) {
+			case keyCode.enter:
+				if ($(this).val() !== '') {
+					onEnter( $(this).val() );
+				}
+				break;
+			case keyCode.arrowUp:
+				onArrowUp();
+				break;
+			case keyCode.arrowDown:
+				onArrowDown();
+				break;
+			case keyCode.leftArrow:
+			case keyCode.rightArrow:
+				$consoleInput.val($consoleInput.val());
+				break;
+		}
+	});
 });
