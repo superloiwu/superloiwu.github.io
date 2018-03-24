82eb32a0526f361b7f2d538bdfeece45cada845f
loi.wu <loi.wu@vipshop.com>
2018-03-24 23:05:19 +0800

    make it better

--- js/index.js
+++ js/index.js
@@ -1,237 +1,136 @@
-$(document).ready(function() {
-  var divx = document.getElementById('terminal');
-  /* Welcome screen */
-  $('#welcome-login').animate({'opacity': '1', 'top': $(window).height()/2 - $('#welcome-login').height()/2 }, 3000);
 
-  setTimeout(function() {
-      $('#login-input').animate({'opacity': '1'}, 3000);
-  }, 4000);
+/* 
 
-  /* player login */
-  var player_name = "Anonymous";
-  var logged = 0;
+Blog: http://yahbee.comlu.com/
+   
+*/
 
-  $('#login-input').keypress(function(e) {
-    if(e.which === 13 && $(this).val() != '') {
-      player_name = $(this).val();
-      logged = 1;
-      $('#welcome-login').animate({'opacity': '0'}, 1000);
-      /* objectives */
-      var help = 0;
-      var list = 0;
-      var connect = 0;
-      var trojan = 0;
-      var disconnect = 0;
+/*===================================================== */
 
-      var won = 0;
-      var able = 0;
 
-      window.setInterval(function() {
-        if (won === 0) {
-          if (help === 1) {
-            $('#objectives #help').css({'color': 'red'});
-            $('#objectives #list').animate({'opacity': '1'}, 1000);
-            if (list === 1) {
-              $('#objectives #list').css({'color': 'red'});
-              $('#objectives #connect').animate({'opacity': '1'}, 1000);
-              if (connect === 1) {
-                $('#objectives #connect').css({'color': 'red'});
-                $('#objectives #trojan').animate({'opacity': '1'}, 1000);
-                if (trojan === 1) {
-                  $('#objectives #trojan').css({'color': 'red'});
-                  $('#objectives #disconnect').animate({'opacity': '1'}, 1000);
-                }
-              }
-            }
-          }
-        }
+//Globals:
+//I'm sorry!
+var CurrentRound = 0;   
+var NightmareMode = false;   
 
-        if (disconnect === 1) {
-          $('#objectives #disconnect').css({'color': 'red'});
-        }
+//if(getURLParameter('DeBuGgOtOlEvEl')){CurrentRound=getURLParameter('DeBuGgOtOlEvEl');} //Debug
+//====================
 
-        if (minutes <= 1 && help === 1 && list === 1 && connect === 0 && trojan === 1 && disconnect === 1) {
-          won++;
-        }
+$( "body" ).on( "click", ".nextround", function() {
+CurrentRound++; //Update the global
+NextRound(CurrentRound);
+});
+$( "body" ).on( "click", ".retry", function() {
+$("html").removeClass();
+NextRound(CurrentRound);
+});
+$( "body" ).on( "click", ".nightmare", function() {
+CurrentRound=5; //Update the global
+NightmareMode=true;
+NextRound(CurrentRound);
+});
 
-        if (won === 1) {
-          $('#objectives li').animate({'opacity': '0'}, 3000);
-          $('#objectives #won').show(3000).animate({'opacity': '1'}, 3000);
-        } else if (won > 1) {
-          won = 2;
-        }
-      }, 1000);
+/*===================================================== */
 
-      /* timer */
-      var seconds = 0;
-      var minutes = 0;
-      var danger = 0;
-      var lost = 0;
 
-      window.setInterval(function() {
-        if (connect === 1) {
-          if (seconds < 10) {
-            $('#time').text('Being traced: ' + minutes + ':0' +seconds);
-          } else {
-            $('#time').text('Being traced: ' + minutes + ':' +seconds);
-          }
-          if (seconds < 59) {
-            seconds++;
-          } else {
-            seconds = 0;
-            minutes++;
-          }
-          if (minutes >= 0 && seconds > 45) {
-            $('#time').css({'color': 'red'});
-            danger++;
-          }
-          if (danger === 1) {
-            $('span').remove();
-            $('#terminal').append('<div>You have only 15 seconds left...<span id="blinking">_</span></div><br>');
-            divx.scrollTop = divx.scrollHeight;
-          } else if (danger > 1) {
-            danger = 2;
-          }
-          if (minutes >= 1 && seconds > 0) {
-            $('#time').text('You\'ve been traced down!');
-            lost++;
-            able = 0;
-          }
-          if (lost === 1) {
-            $('span').remove();
-            $('#terminal').append('<div>You\'ve been traced down!<br>Formatting HDD...<br>Goodbye...<span id="blinking">_</span></div><br>');
-            $('#objectives li').animate({'opacity': '0'}, 3000);
-            $('#objectives #lost').show(3000).animate({'opacity': '1'}, 3000);
-            divx.scrollTop = divx.scrollHeight;
-            $('#objectives li').css({'color': 'green'}).animate({'opacity': '1'}, 3000);
-            setTimeout(function() {
-              $('#terminal').animate({'opacity': '0'}, 1000);
-              $('#root').animate({'opacity': '0'}, 1000);
-              $('input').animate({'opacity': '0'}, 1000).hide(3000);
-              $('#objectives').animate({'opacity': '0'}, 1000);
-              $('#time').animate({'opacity': '0'}, 1000);
-              help = 0;
-              list = 0;
-              connect = 0;
-              trojan = 0;
-              disconnect = 0;
-              won = 0;
-              $('#objectives li').css({'color': 'green'});
-            }, 20000);
-          } else if (lost > 1) {
-            lost = 2;
-          }
-        }
-      }, 1000);
+function NextRound(CurrentRoundArg){
+$('#roundcheck').html(CurrentRound);
+CurrentRoundArg +=5; //start on 3 letters
+var Fakers = CurrentRoundArg*5;
+$('#terminal').hide().html(BuildPasswords(makeid(CurrentRoundArg), Fakers)).fadeIn();
+}
 
-      setTimeout(function(){
-        $('#terminal').animate({'opacity': '1'}, 1000);
-      }, 1000);
+function Shuffle(o) {
+  for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
+};
 
-      setTimeout(function(){
-        $('#root').animate({'opacity': '1'}, 1000);
-        $('input').animate({'opacity': '1'}, 1000).show();
-      }, 5000);
+function makeid(leength) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //abcdefghijklmnopqrstuvwxyz0123456789";
+  if(NightmareMode==true){var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";}
+  for (var i = 0; i < leength; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
 
-      setTimeout(function(){
-        $('span').remove();
-        $('#terminal').append('<br><div>Welcome to HACKSYS [version 1.0.0].<br>(c) Copyright 2014 HACorp Corporation. All rights reserved.<br><br>' + player_name + ' authenticated.<br>HACKSYS ready for use.<br>Use the help command for a list of commands.<br><br><span id="initial-root">$</span>&nbsp;&nbsp;&nbsp;<span id="blinking">_</span></div>');
-      }, 6500);
 
-      setTimeout(function() {
-        $('#time').animate({'opacity': '1'}, 3000);
-        $('#objectives').animate({'opacity': '1'}, 3000);
-      }, 7000);
+function IsChar(charr, Haystack) {
+  if (Haystack.indexOf(charr) === -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
-      setTimeout(function() {
-        $('#objectives #help').animate({'opacity': '1'}, 500);
-        able = 1;
-      }, 10000);
-
-      $('#input').keypress(function(e) {
-          if(e.which === 13 && $(this).val() != '' && able === 1) {
-            $('div span').remove();
-            $('initial-root').remove();
-
-            /* 
-             * List of commands available in the game:
-             * 
-             * help
-             * list
-             * connect nearest-phone-relay
-             * send trojan
-             * send adware
-             * disconnect
-             * exit
-             * 
-             */
-
-            switch( $(this).val() ) {
-              case 'help':
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>List of commands:<br>help - Show this message (command list)<br>connect [DNS name] - Connects host computer with the targeted DNS name<br>disconnect - Disconnects host computer from the connected DNS name<br>exit - Shuts down host computer<br>list - Shows the list of the currently available applications<br>send [application type] - Sends given application name to the connected DNS name<span id="blinking">_</span></div>');
-                help = 1;
-                break;
-              case 'connect nearest.phone.relay':
-                if (connect === 0) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Connecting...<br>Connected.<br>You have 1 minute before you get traced down.<span id="blinking">_</span></div>');
-                  connect = 1;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are already connected to nearest.phone.relay.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'list':
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>List of currently available applications:<br>AdBreak - bombard targeted computer with advertisement pop-ups (type: adware)<br>Trojan.Vaklik.BBB - steals vital information from the targeted computer (type: trojan)<span id="blinking">_</span>');
-                list = 1;
-                break;
-              case 'send trojan':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Sending Trojan.Vaklik.BBB...<br>Trojan.Vaklik.BBB sent.<span id="blinking">_</span></div>');
-                  trojan = 1;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'send adware':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Sending AdBreak...<br>Unable to send AdBreak.<span id="blinking">_</span></div>');
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'disconnect':
-                if (connect === 1) {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Disconnecting...<br>Disconnected<span id="blinking">_</span></div>');
-                  disconnect = 1;
-                  connect = 0;
-                  minutes = 0;
-                  seconds = 0;
-                } else {
-                  $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>You are not connected to any DNS name.<span id="blinking">_</span></div>');
-                }
-                break;
-              case 'exit':
-                $('#terminal').animate({'opacity': '0'}, 1000);
-                $('#root').animate({'opacity': '0'}, 1000);
-                $('input').animate({'opacity': '0'}, 1000).hide(3000);
-                $('#objectives').animate({'opacity': '0'}, 1000);
-                $('#time').animate({'opacity': '0'}, 1000);
-                help = 0;
-                list = 0;
-                connect = 0;
-                trojan = 0;
-                disconnect = 0;
-                won = 0;
-                $('#objectives li').css({'color': 'green'});
-                break;
-              default:
-                $('#terminal').append('<div>$&nbsp;&nbsp;&nbsp;' + $(this).val() + '<br>Command not recognized. Type in help for a list of commands.<span id="blinking">_</span></div>');
-            }
-            $('#terminal').append('<br>');
-
-            divx.scrollTop = divx.scrollHeight;
-            $(this).val('');
-          }
-      });
+function MatchWords(Needle, Haystack) {
+  var Counter = 0;
+  for (var i = 0, len = Needle.length; i < len; i++) { //for each letter in needle
+    if (IsChar(Needle[i], Haystack)) { //is the letter in haystack?
+      Counter++; //letter is in haystack
     }
-  });
+  }
+  return Counter;
+}
+
+function CreatePasswords(Actual, Fakes) {
+  var OutArray = [Actual]
+  var leength = Actual.length;
+  for (var i = 0; i < Fakes; i++) { //for amount of fakes
+	var Faker = makeid(leength);
+	if(MatchWords(Actual, Faker) == leength){i--;}//Uh oh, all the letters match, can't have
+	else {OutArray.push(Faker);} //Add a fake
+  }
+  return Shuffle(OutArray); //shuffle the deck
+}
+
+function MakeHTML(input, Actual) {
+  var AreaID = makeid(10);
+  var Output = '<b><span id="' + AreaID + '-attempts">[#] [#] [#] [#] [#]</span> ATTEMPTS REMAIN BEFORE TERMINAL LOCK OUT</b>' +
+    '<br><b id="' + AreaID + '-matches"></b>' +
+    '<br><br> !# System.Root.$~admin -access passwords:<br>'; //insert any foreword you want here.
+  var Matches = 0;
+  var AddHtml = 'href="javascript:void(0)"'; // Additional HTML to throw in there, eg. Any onclick events & Stuff
+  for (var i = 0; i < input.length; i++) {
+    Matches = MatchWords(Actual, input[i]);
+    Output+='<a '+AddHtml+' data-string="'+input[i]+'" data-actual="'+Actual+'" data-matches="'+Matches+'" data-areaid="'+AreaID+'" class="password_link" rel="'+Matches+'">'+input[i]+'</a> ';
+//onclick="CheckPassword(\''+input[i]+'\',\''+Actual+'\',\''+Matches+'\',\''+AreaID+'\');"
+
+  }
+  Output += ''; //insert any afterword you want here
+  return Output;
+}
+
+function BuildPasswords(Actual, Fakes) {
+  var Array = CreatePasswords(Actual, Fakes);
+  return MakeHTML(Array, Actual);
+}
+
+function CheckPassword(Given, Actual, Matches, AreaID) {
+  var attempts = $('#' + AreaID + '-attempts');
+  var matchess = $('#' + AreaID + '-matches');
+  if (Given == Actual) {
+	StingPage('green', 250);
+    matchess.hide().html(matchess.html() + '<br><span>' + Matches + '</span> / ' + Actual.length + '  Matched [' + Given + '] ' + 'SUCCESS!<br>Next Screen loading in 3 seconds..').fadeIn();
+    setTimeout(function(){
+      CurrentRound++; //Update the global
+      NextRound(CurrentRound);}, 2500);
+	return 'Success!';
+  } else {
+    attempts.hide().html(attempts.text().substr(4)).fadeIn();
+    if (attempts.text().length == 0) {StingPage('red', 'forever'); $('#terminal').hide().html('<h3 style="display: inline-block;">TERMINAL LOCKED</h3> <div class="modal"><a href="#YouFailure" class="retry">Retry Screen: '+CurrentRound+'</a></div>').fadeIn();}
+	else {StingPage('red', 250); matchess.hide().html(matchess.html() + '<br><span>' + Matches + '</span> / ' + Actual.length + '  Matched [' + Given + ']').fadeIn();}
+    return false;
+  }
+}
+
+function StingPage(color, time){
+    $('html').addClass('sting-'+color+' sting'); // class must exist, eg. .sting-red
+	if(time != 'forever'){
+    setTimeout(function(){
+      $('html').removeClass('sting-'+color);}, time);}
+}
+
+$( "body" ).on( "click", ".password_link", function() {
+CheckPassword($(this).data('string'),$(this).data('actual'),$(this).data('matches'),$(this).data('areaid'));
 });
